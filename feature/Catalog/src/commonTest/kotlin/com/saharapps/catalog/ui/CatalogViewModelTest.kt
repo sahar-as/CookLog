7e8ee16f6d98a6be1bfb257f49c8e3d5f3991629
Sahar Asadian <sahar.asadian90@gmail.com>
2026-05-15 17:27:42 +0330

    Feat: Writing test for CatalogViewModel.kt

--- feature/Catalog/src/commonTest/kotlin/com/saharapps/catalog/ui/CatalogViewModelTest.kt
+++ feature/Catalog/src/commonTest/kotlin/com/saharapps/catalog/ui/CatalogViewModelTest.kt
@@ -1,12 +1,16 @@
 package com.saharapps.catalog.ui
 
+import app.cash.turbine.test
+import com.saharapps.catalog.CatalogItem
 import com.saharapps.catalog.usecase.FakeDeleteCatalogUseCase
 import com.saharapps.catalog.usecase.FakeGetCatalogUseCase
 import com.saharapps.catalog.usecase.FakeSaveCatalogUseCase
+import com.saharapps.ui.ViewStatus
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import kotlin.test.*
 
@@ -38,5 +42,52 @@ class CatalogViewModelTest {
     @AfterTest
     fun tearDown() {
         Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `getCatalogs should update state to SUCCESS when use case returns data`() = runTest {
+        // Arrange
+        val mockCatalogs = listOf(
+            CatalogItem(
+                id = 1,
+                name = "Test Item",
+                imagePath = ""
+            )
+        )
+
+        getCatalogUseCase.result = Result.success(mockCatalogs)
+
+        // Act & Assert
+        viewModel.catalogUiState.test {
+            assertEquals(ViewStatus.INITIAL, awaitItem().viewStatus)
+
+            viewModel.getCatalogs()
+            assertEquals(ViewStatus.LOADING, awaitItem().viewStatus)
+
+            testDispatcher.scheduler.advanceTimeBy(501)
+            val successState = awaitItem()
+            assertEquals(ViewStatus.SUCCESS, successState.viewStatus)
+            assertEquals(mockCatalogs, successState.catalogs)
+        }
+    }
+
+    @Test
+    fun `getCatalogs should update state to FAILED when use case fails`() = runTest {
+        // ARRANGE
+        val errorMessage = "Network Error"
+        getCatalogUseCase.result = Result.failure(Exception(errorMessage))
+
+        //ACT & ASSERT
+        viewModel.catalogUiState.test {
+            assertEquals(ViewStatus.INITIAL, awaitItem().viewStatus)
+
+            viewModel.getCatalogs()
+            assertEquals(ViewStatus.LOADING, awaitItem().viewStatus)
+
+            testDispatcher.scheduler.advanceTimeBy(501)
+            val failedState = awaitItem()
+            assertEquals(ViewStatus.FAILED, failedState.viewStatus)
+            assertEquals(errorMessage, failedState.failedMessage)
+        }
     }
 }
